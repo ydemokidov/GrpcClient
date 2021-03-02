4aa1e59b3ec6e427686d3535781ab3663da4831f
ydemokidov <ydemokidov@gmail.com>
2021-03-03 01:09:26 +0300

    Initial commit

---
+++ src/main/java/org/example/GrpcClient.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.example.grpc.GreetingServiceGrpc;
+import org.example.grpc.GreetingServiceOuterClass;
+
+public class GrpcClient {
+    public static void main(String[] args){
+        //создадим канал для соединения с сервером
+        ManagedChannel channel = ManagedChannelBuilder
+                                    .forTarget("localhost:5454")
+                                    .usePlaintext()
+                                    .build();
+
+        // stub - умеет делать удаленные вызовы на сервере
+
+        GreetingServiceGrpc.GreetingServiceBlockingStub blockingStub = GreetingServiceGrpc.newBlockingStub(channel);
+
+        //создаем запрос
+        GreetingServiceOuterClass.HelloRequest request = GreetingServiceOuterClass
+                                                            .HelloRequest
+                                                            .newBuilder()
+                                                            .setName("Georges")
+                                                            .build();
+        //удаленный вызов процедуры на сервере
+        GreetingServiceOuterClass.HelloResponse response = blockingStub.greeting(request);
+
+        System.out.println(response);
+
+        channel.shutdownNow();
+    }
+}
